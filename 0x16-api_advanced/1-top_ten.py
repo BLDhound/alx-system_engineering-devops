aea98959d987e491633cd0b77e5527810c5d8d96
BLDhound <lekarrikaila567@gmail.com>
2023-05-09 06:50:47 +0000

    recurse it

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-"""
-1-main
-"""
-import sys
+"""Top ten"""
+import requests
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/.json?limit=10".format(subreddit)
+    headers = {'user-agent': 'MyAPI/0.0.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        try:
+            data = response.json().get("data")
+            children = data.get("children")
+            for each in children[:10]:
+                print(each.get("data").get("title"))
+        except Exception as e:
+            print(None)
     else:
-        top_ten(sys.argv[1])
+        print(None)
